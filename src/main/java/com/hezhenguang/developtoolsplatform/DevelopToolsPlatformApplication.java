6c81aa857b7fb57b3b664eb3233c5add31d713cc
feiyu10 <1416669659@qq.com>
2021-08-08 00:08:00 +0800

    20210808  git初始化

---
+++ src/main/java/com/hezhenguang/developtoolsplatform/DevelopToolsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.hezhenguang.developtoolsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopToolsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopToolsPlatformApplication.class, args);
+    }
+
+}
