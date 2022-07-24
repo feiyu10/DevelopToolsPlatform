95c0079f8a57fc12d3b127f5a63e93eb358aedd7
feiyu10 <1416669659@qq.com>
2022-07-24 23:08:15 +0800

    20220704  更新

---
+++ src/main/java/com/hezhenguang/developtoolsplatform/study/test/ByteDanceTest1.java
@@ -0,0 +1,17 @@
+package com.hezhenguang.developtoolsplatform.study.test;
+
+/**
+ * 系统版本: v1.0<br>
+ *
+ * @description:
+ * @author: hezg<br>
+ * @create: 2022-06-08
+ **/
+public class ByteDanceTest1 {
+
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
+
+
+}
---
+++ src/main/java/com/hezhenguang/developtoolsplatform/study/test/HuoLaLaTest1.java
@@ -0,0 +1,15 @@
+package com.hezhenguang.developtoolsplatform.study.test;
+
+/**
+ * 系统版本: v1.0<br>
+ *
+ * @description:
+ * @author: hezg26906@hundsun.com<br>
+ * @create: 2022-06-28
+ **/
+public class HuoLaLaTest1 {
+
+
+
+
+}
